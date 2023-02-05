b0e4ce6c135a36d9f4911a0a847a234eafe5e872
Simon Ionashku <simon.ionashku@gmail.com>
2023-02-05 17:48:40 -0500

    [08] - Configurations

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -1,12 +1,51 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+/**
+ * @format
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+require("dotenv").config();
+require("@nomiclabs/hardhat-waffle");
+
 module.exports = {
   solidity: {
-    version: '0.8.9',
+    version: "0.8.9",
     settings: {
       optimizer: {
         enabled: true,
         runs: 200,
       },
     },
+  },
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {},
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*", // Match any network id
+    },
+    // rinkeby: {
+    //   url: `${process.env.NEXT_PUBLIC_ALCHEMY_RPC_URL}`,
+    //   accounts: [`0x${process.env.METAMASK_PRIVATE_KEY}`]
+    // },
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`0x${process.env.METAMASK_PRIVATE_KEY}`],
+    },
+    // mumbai: {
+    //   url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+    //   accounts: [`0x${process.env.METAMASK_PRIVATE_KEY}`]
+    // },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  mocha: {
+    timeout: 40000,
+  },
+  etherscan: {
+    apiKey: `${process.env.ETHERSCAN_API_KEY}`,
   },
 };
