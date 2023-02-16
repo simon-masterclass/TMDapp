9ed04ff51fca806ba095e61e14e6e6177e59d89b
Simon Ionashku <simon.ionashku@gmail.com>
2023-02-15 23:22:36 -0500

    [16] - Nextify components

--- client/src/pages/components/DisplayCampaigns.jsx
+++ client/src/pages/components/DisplayCampaigns.jsx
@@ -1,23 +1,32 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+/** @format */
 
-import FundCard from './FundCard';
-import { loader } from '../assets';
+import React from "react";
+//import { useNavigate } from 'react-router-dom';
+import { useRouter } from "next/router";
+
+import FundCard from "./FundCard";
+import { loader } from "../assets";
 
 const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
-  const navigate = useNavigate();
+  const router = useRouter();
 
   const handleNavigate = (campaign) => {
-    navigate(`/campaign-details/${campaign.title}`, { state: campaign })
-  }
-  
+    router.push(`/campaign-details/${campaign.title}`, { state: campaign });
+  };
+
   return (
     <div>
-      <h1 className="font-epilogue font-semibold text-[18px] text-white text-left">{title} ({campaigns.length})</h1>
+      <h1 className="font-epilogue font-semibold text-[18px] text-white text-left">
+        {title} ({campaigns.length})
+      </h1>
 
       <div className="flex flex-wrap mt-[20px] gap-[26px]">
         {isLoading && (
-          <img src={loader} alt="loader" className="w-[100px] h-[100px] object-contain" />
+          <img
+            src={loader}
+            alt="loader"
+            className="w-[100px] h-[100px] object-contain"
+          />
         )}
 
         {!isLoading && campaigns.length === 0 && (
@@ -26,14 +35,18 @@ const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
           </p>
         )}
 
-        {!isLoading && campaigns.length > 0 && campaigns.map((campaign) => <FundCard 
-          key={campaign.id}
-          {...campaign}
-          handleClick={() => handleNavigate(campaign)}
-        />)}
+        {!isLoading &&
+          campaigns.length > 0 &&
+          campaigns.map((campaign) => (
+            <FundCard
+              key={campaign.id}
+              {...campaign}
+              handleClick={() => handleNavigate(campaign)}
+            />
+          ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DisplayCampaigns
+export default DisplayCampaigns;
