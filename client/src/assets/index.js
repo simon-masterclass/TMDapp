07ce3e1671afa826f4bed41ed8bb9745ca72a874
Simon Ionashku <simon.ionashku@gmail.com>
2023-02-21 23:55:33 -0500

    [24] - Zero Army Logo

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,17 +1,21 @@
-import createCampaign from './create-campaign.svg';
-import dashboard from './dashboard.svg';
-import logo from './logo.svg';
-import logout from './logout.svg';
-import payment from './payment.svg';
-import profile from './profile.svg';
-import sun from './sun.svg';
-import withdraw from './withdraw.svg';
-import tagType from './type.svg';
-import search from './search.svg';
-import menu from './menu.svg';
-import money from './money.svg';
-import loader from './loader.svg';
-import thirdweb from './thirdweb.png';
+/** @format */
+
+import createCampaign from "./create-campaign.svg";
+import dashboard from "./dashboard.svg";
+import logo from "./logo.svg";
+import logout from "./logout.svg";
+import payment from "./payment.svg";
+import profile from "./profile.svg";
+import sun from "./sun.svg";
+import withdraw from "./withdraw.svg";
+import tagType from "./type.svg";
+import search from "./search.svg";
+import menu from "./menu.svg";
+import money from "./money.svg";
+import loader from "./loader.svg";
+import thirdweb from "./thirdweb.png";
+import zarmy100x100 from "./zarmy100x100.png";
+import zarmy50x50 from "./zarmy50x50.png";
 
 export {
   tagType,
@@ -28,4 +32,6 @@ export {
   money,
   loader,
   thirdweb,
+  zarmy100x100,
+  zarmy50x50,
 };
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,38 +1,59 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+/** @format */
 
-import { logo, sun } from '../assets';
-import { navlinks } from '../constants';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import { logo, sun, zarmy100x100, zarmy50x50 } from "../assets";
+import { navlinks } from "../constants";
 
 const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
-  <div className={`w-[48px] h-[48px] rounded-[10px] ${isActive && isActive === name && 'bg-[#2c2f32]'} flex justify-center items-center ${!disabled && 'cursor-pointer'} ${styles}`} onClick={handleClick}>
+  <div
+    className={`w-[48px] h-[48px] rounded-[10px] ${
+      isActive && isActive === name && "bg-[#2c2f32]"
+    } flex justify-center items-center ${
+      !disabled && "cursor-pointer"
+    } ${styles}`}
+    onClick={handleClick}>
     {!isActive ? (
-      <img src={imgUrl} alt="fund_logo" className="w-1/2 h-1/2" />
+      <img
+        src={imgUrl}
+        alt="fund_logo"
+        className="w-1/2 h-1/2"
+      />
     ) : (
-      <img src={imgUrl} alt="fund_logo" className={`w-1/2 h-1/2 ${isActive !== name && 'grayscale'}`} />
+      <img
+        src={imgUrl}
+        alt="fund_logo"
+        className={`w-1/2 h-1/2 ${isActive !== name && "grayscale"}`}
+      />
     )}
   </div>
-)
+);
 
 const Sidebar = () => {
   const navigate = useNavigate();
-  const [isActive, setIsActive] = useState('dashboard');
+  const [isActive, setIsActive] = useState("dashboard");
 
   return (
     <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh]">
       <Link to="/">
-        <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
+        <div className="w-[107px] h-[107px] bg-[#2c2f32] rounded-full flex justify-center items-center cursor-pointer">
+          <img
+            className="rounded-full object-contain" // bg-[#2c2f32]
+            src={zarmy100x100}
+          />
+        </div>
       </Link>
 
       <div className="flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12">
         <div className="flex flex-col justify-center items-center gap-3">
           {navlinks.map((link) => (
-            <Icon 
+            <Icon
               key={link.name}
               {...link}
               isActive={isActive}
               handleClick={() => {
-                if(!link.disabled) {
+                if (!link.disabled) {
                   setIsActive(link.name);
                   navigate(link.link);
                 }
@@ -41,10 +62,13 @@ const Sidebar = () => {
           ))}
         </div>
 
-        <Icon styles="bg-[#1c1c24] shadow-secondary" imgUrl={sun} />
+        <Icon
+          styles="bg-[#1c1c24] shadow-secondary"
+          imgUrl={sun}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
